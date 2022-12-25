02eb3b484413e868360b3a3cabeece9583a5a3e0
ezoltech <ezramamo13@gmail.com>
2022-12-25 10:33:54 -0800

    fiished-for now

--- QR-Code-Generator/Form1.Designer.cs
+++ QR-Code-Generator/Form1.Designer.cs
@@ -29,18 +29,18 @@ namespace QR_Code_Generator
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txt_url = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pic_gen = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_gen)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_url
             // 
-            this.txt_url.Location = new System.Drawing.Point(62, 52);
+            this.txt_url.Location = new System.Drawing.Point(62, 36);
             this.txt_url.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_url.Multiline = true;
             this.txt_url.Name = "txt_url";
@@ -50,7 +50,7 @@ namespace QR_Code_Generator
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(104, 15);
+            this.label1.Location = new System.Drawing.Point(118, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(209, 23);
@@ -60,7 +60,7 @@ namespace QR_Code_Generator
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(350, 52);
+            this.button1.Location = new System.Drawing.Point(350, 36);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(133, 33);
@@ -72,29 +72,41 @@ namespace QR_Code_Generator
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 54);
+            this.label2.Location = new System.Drawing.Point(9, 38);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 23);
             this.label2.TabIndex = 3;
             this.label2.Text = "URL";
             // 
-            // pictureBox1
+            // pic_gen
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(153, 114);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(143, 136);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.pic_gen.BackColor = System.Drawing.Color.White;
+            this.pic_gen.Location = new System.Drawing.Point(100, 75);
+            this.pic_gen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pic_gen.Name = "pic_gen";
+            this.pic_gen.Size = new System.Drawing.Size(205, 184);
+            this.pic_gen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pic_gen.TabIndex = 4;
+            this.pic_gen.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(350, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 261);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(489, 272);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pic_gen);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -105,7 +117,7 @@ namespace QR_Code_Generator
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QR-CODE-GENERATOR";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_gen)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,7 +129,8 @@ namespace QR_Code_Generator
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pic_gen;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- QR-Code-Generator/Form1.cs
+++ QR-Code-Generator/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QRCoder;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,14 +20,24 @@ namespace QR_Code_Generator
 
         private void button1_Click(object sender, EventArgs e)
         {
+           
             if(txt_url.Text == "")
             {
                 MessageBox.Show("Nothing To Generate, please inseert or, paste your url inorder to generate your QR-CODE");
             }
             else
             {
-
+                QRCodeGenerator qr = new QRCodeGenerator();
+                QRCodeData data = qr.CreateQrCode(txt_url.Text, QRCodeGenerator.ECCLevel.Q);
+                QRCode code = new QRCode(data);
+                pic_gen.Image = code.GetGraphic(5);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt_url.Text = "";
+            pic_gen.Image = null;
         }
     }
 }
